9db2fd54d544d3e4206338e3c943c71b642826a6
DevSrSouza <devsrsouza@gmail.com>
2023-04-04 11:04:20 -0300

    fix indentation

--- src/commonMain/kotlin/wizard/files/RootBuildGradleKts.kt
+++ src/commonMain/kotlin/wizard/files/RootBuildGradleKts.kt
@@ -6,20 +6,20 @@ class RootBuildGradleKts(info: ProjectInfo) : ProjectFile {
     override val path = "build.gradle.kts"
     override val content = buildString {
         appendLine("plugins {")
-        appendLine("        kotlin(\"multiplatform\").version(libs.versions.kotlin.get()).apply(false)")
-        appendLine("        kotlin(\"native.cocoapods\").version(libs.versions.kotlin.get()).apply(false)")
+        appendLine("    kotlin(\"multiplatform\").version(libs.versions.kotlin.get()).apply(false)")
+        appendLine("    kotlin(\"native.cocoapods\").version(libs.versions.kotlin.get()).apply(false)")
         if(info.hasAndroid) {
-            appendLine("        kotlin(\"android\").version(libs.versions.kotlin.get()).apply(false)")
-            appendLine("        alias(libs.plugins.android.application).apply(false)")
-            appendLine("        alias(libs.plugins.android.library).apply(false)")
+            appendLine("    kotlin(\"android\").version(libs.versions.kotlin.get()).apply(false)")
+            appendLine("    alias(libs.plugins.android.application).apply(false)")
+            appendLine("    alias(libs.plugins.android.library).apply(false)")
         }
-        appendLine("        alias(libs.plugins.compose).apply(false)")
+        appendLine("    alias(libs.plugins.compose).apply(false)")
 
         info.dependencies.filter { it.isPlugin() }.forEach { dep ->
             if (dep == KotlinxSerializationPlugin) {
-                appendLine("        ${dep.pluginNotation()}.version(libs.versions.kotlin.get()).apply(false)")
+                appendLine("    ${dep.pluginNotation()}.version(libs.versions.kotlin.get()).apply(false)")
             } else {
-                appendLine("        alias(libs.plugins.${dep.versionCatalog.libraryAccessor}).apply(false)")
+                appendLine("    alias(libs.plugins.${dep.versionCatalog.libraryAccessor}).apply(false)")
             }
         }
 
