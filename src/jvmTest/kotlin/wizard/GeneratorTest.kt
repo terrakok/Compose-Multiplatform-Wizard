c59078d4513ab7617ecfab1354748eaa2d5bcf52
c3smonkey <c3smonkey@gmail.com>
2023-09-23 07:42:25 +0200

    chore: change JDK 1.8 to JDK 17

--- src/jvmTest/kotlin/wizard/GeneratorTest.kt
+++ src/jvmTest/kotlin/wizard/GeneratorTest.kt
@@ -78,7 +78,7 @@ class GeneratorTest {
                     androidTarget {
                         compilations.all {
                             kotlinOptions {
-                                jvmTarget = "1.8"
+                                jvmTarget = "17"
                             }
                         }
                     }
@@ -184,8 +184,8 @@ class GeneratorTest {
                         res.srcDirs("src/androidMain/resources")
                     }
                     compileOptions {
-                        sourceCompatibility = JavaVersion.VERSION_1_8
-                        targetCompatibility = JavaVersion.VERSION_1_8
+                        sourceCompatibility = JavaVersion.VERSION_17
+                        targetCompatibility = JavaVersion.VERSION_17
                     }
                 }
 
@@ -358,7 +358,7 @@ class GeneratorTest {
                     androidTarget {
                         compilations.all {
                             kotlinOptions {
-                                jvmTarget = "1.8"
+                                jvmTarget = "17"
                             }
                         }
                     }
@@ -406,8 +406,8 @@ class GeneratorTest {
                         res.srcDirs("src/androidMain/resources")
                     }
                     compileOptions {
-                        sourceCompatibility = JavaVersion.VERSION_1_8
-                        targetCompatibility = JavaVersion.VERSION_1_8
+                        sourceCompatibility = JavaVersion.VERSION_17
+                        targetCompatibility = JavaVersion.VERSION_17
                     }
                 }
 
