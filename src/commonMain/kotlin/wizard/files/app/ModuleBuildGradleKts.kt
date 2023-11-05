c0fd6a326c36a2b592d35bb1e8886141230a3ac6
Artsiom Komar <kom07art@gmail.com>
2023-11-05 16:09:04 +0100

    Added compose to android build features (#38)
    Fixes issue with preview on Android out of the box

--- src/commonMain/kotlin/wizard/files/app/ModuleBuildGradleKts.kt
+++ src/commonMain/kotlin/wizard/files/app/ModuleBuildGradleKts.kt
@@ -196,6 +196,9 @@ class ModuleBuildGradleKts(info: ProjectInfo) : ProjectFile {
             appendLine("        sourceCompatibility = JavaVersion.VERSION_17")
             appendLine("        targetCompatibility = JavaVersion.VERSION_17")
             appendLine("    }")
+            appendLine("    buildFeatures {")
+            appendLine("        compose = true")
+            appendLine("    }")
             appendLine("}")
             appendLine("")
         }
