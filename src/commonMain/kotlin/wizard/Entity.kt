8539a05ffc283d9c47b11a07aa26ac4d251098f1
Konstantin Tskhovrebov <terrakok@gmail.com>
2024-09-16 13:44:41 +0200

    Update versions

--- src/commonMain/kotlin/wizard/Entity.kt
+++ src/commonMain/kotlin/wizard/Entity.kt
@@ -25,7 +25,7 @@ data class ProjectInfo(
     val name: String,
     val moduleName: String,
     val platforms: Set<ProjectPlatform>,
-    val gradleVersion: String = "8.10.1",
+    val gradleVersion: String = "8.10",
     val androidMinSdk: Int = 21,
     val androidTargetSdk: Int = 35,
     val dependencies: Set<Dependency>
