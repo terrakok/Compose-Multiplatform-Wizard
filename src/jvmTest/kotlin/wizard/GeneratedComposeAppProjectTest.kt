9e00ef060999eeaa40edc89ba45bbc7cdbe32a60
Konstantin Tskhovrebov <terrakok@gmail.com>
2024-11-28 22:01:40 +0100

    Add dependency version stability check in test setup

--- src/jvmTest/kotlin/wizard/GeneratedComposeAppProjectTest.kt
+++ src/jvmTest/kotlin/wizard/GeneratedComposeAppProjectTest.kt
@@ -285,7 +285,19 @@ class GeneratedComposeAppProjectTest {
                         plugins {
                             id("com.github.ben-manes.versions").version("0.51.0")
                     """.trimIndent()
-                )
+                ) + """
+                    |
+                    |tasks.named<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask>("dependencyUpdates") {
+                    |    rejectVersionIf { isNonStable(candidate.version) }
+                    |}
+                    |
+                    |fun isNonStable(version: String): Boolean {
+                    |    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+                    |    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+                    |    val isStable = stableKeyword || regex.matches(version)
+                    |    return isStable.not()
+                    |}
+                """.trimMargin()
             )
         }
         checkCommand(
