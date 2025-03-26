0a22952dea5f69fc27947e20b02477bf4d7b168f
Konstantin Tskhovrebov <terrakok@gmail.com>
2025-03-26 14:32:47 +0100

    Update dependency versions across the project

--- src/jvmTest/kotlin/wizard/GeneratedComposeAppProjectTest.kt
+++ src/jvmTest/kotlin/wizard/GeneratedComposeAppProjectTest.kt
@@ -283,7 +283,7 @@ class GeneratedComposeAppProjectTest {
                     "plugins {",
                     """
                         plugins {
-                            id("com.github.ben-manes.versions").version("0.51.0")
+                            id("com.github.ben-manes.versions").version("0.52.0")
                     """.trimIndent()
                 ) + """
                     |
