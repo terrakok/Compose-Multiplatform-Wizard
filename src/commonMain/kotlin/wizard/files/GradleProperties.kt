ed886d5cc3dc2063d25bf81c879f3c455528de63
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2025-12-01 18:00:02 +0100

    Clean up `gradle.properties`: remove unused and outdated entries.

--- src/commonMain/kotlin/wizard/files/GradleProperties.kt
+++ src/commonMain/kotlin/wizard/files/GradleProperties.kt
@@ -17,21 +17,16 @@ class GradleProperties(info: ProjectInfo) : ProjectFile {
             org.gradle.parallel=true
 
             #Kotlin
-            kotlin.code.style=official
             kotlin.daemon.jvmargs=-Xmx4G
             kotlin.native.binary.gc=cms
             kotlin.incremental.wasm=true
 
             #Android
             android.useAndroidX=true
-            android.nonTransitiveRClass=true
         """.trimIndent())
         if (info.hasPlatform(ProjectPlatform.Js) || info.hasPlatform(ProjectPlatform.Macos)) {
             appendLine("")
             appendLine("#Compose")
-            if (info.hasPlatform(ProjectPlatform.Js)) {
-                appendLine("org.jetbrains.compose.experimental.jscanvas.enabled=true")
-            }
             if (info.hasPlatform(ProjectPlatform.Macos)) {
                 appendLine("org.jetbrains.compose.experimental.macos.enabled=true")
             }
