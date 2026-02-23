a61e457d786318fefd31a4f703c6397b959d3db5
Lucas Ginard <57275367+LucasGinard@users.noreply.github.com>
2026-02-22 23:41:45 -0300

    Refactor WebIndexHtml content initialization remove appendLine

--- src/commonMain/kotlin/wizard/files/composeApp/webApp/IndexHtml.kt
+++ src/commonMain/kotlin/wizard/files/composeApp/webApp/IndexHtml.kt
@@ -5,39 +5,45 @@ import wizard.ProjectInfo
 
 class WebIndexHtml(info: ProjectInfo) : ProjectFile {
     override val path = "webApp/src/commonMain/resources/index.html"
-    override val content = buildString {
-        appendLine("""<!doctype html>""")
-        appendLine("""<html lang="en">""")
-        appendLine("""    <head>""")
-        appendLine("""        <meta charset="UTF-8">""")
-        appendLine("""        <meta name="viewport" content="width=device-width, initial-scale=1.0">""")
-        appendLine("""        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">""")
-        appendLine("""        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">""")
-        appendLine("""        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">""")
-        appendLine("""        <link rel="manifest" href="./manifest.json">""")
-        appendLine("""        <title>${info.name}</title>""")
-        appendLine("""        <style>""")
-        appendLine("""            html,""")
-        appendLine("""            body {""")
-        appendLine("""                width: 100%;""")
-        appendLine("""                height: 100%;""")
-        appendLine("""                margin: 0;""")
-        appendLine("""                padding: 0;""")
-        appendLine("""                overflow: hidden;""")
-        appendLine("""            }""")
-        appendLine("""        </style>""")
-        appendLine("""    </head>""")
-        appendLine("""    <body style="text-align: center; align-content: center">""")
-        appendLine("""    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 50 50" role="presentation">""")
-        appendLine("""        <circle cx="25" cy="25" r="20" stroke="#ccc" stroke-width="4" fill="none"/>""")
-        appendLine("""        <circle cx="25" cy="25" r="20" stroke="#333" stroke-width="4" fill="none" stroke-linecap="round" stroke-dasharray="90 125">""")
-        appendLine("""            <animateTransform attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="1s" repeatCount="indefinite"/>""")
-        appendLine("""        </circle>""")
-        appendLine("""    </svg>""")
-        appendLine("""    </body>""")
-        appendLine("""    <script src="webApp.js"></script>""")
-        appendLine("""</html>""")
-    }
+    override val content = """
+        <!doctype html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+                <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+                <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+                <link rel="manifest" href="./manifest.json">
+                <title>${info.name}</title>
+                <style>
+                    html,
+                    body {
+                        width: 100%;
+                        height: 100%;
+                        margin: 0;
+                        padding: 0;
+                        overflow: hidden;
+                    }
+                </style>
+            </head>
+            <body style="text-align: center; align-content: center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 50 50" role="presentation">
+                    <circle cx="25" cy="25" r="20" stroke="#ccc" stroke-width="4" fill="none"/>
+                    <circle cx="25" cy="25" r="20" stroke="#333" stroke-width="4" fill="none"
+                            stroke-linecap="round" stroke-dasharray="90 125">
+                        <animateTransform attributeName="transform"
+                                          type="rotate"
+                                          from="0 25 25"
+                                          to="360 25 25"
+                                          dur="1s"
+                                          repeatCount="indefinite"/>
+                    </circle>
+                </svg>
+                <script src="webApp.js"></script>
+            </body>
+        </html>
+    """.trimIndent()
 }
 
 class WebManifestJson(info: ProjectInfo) : ProjectFile {
